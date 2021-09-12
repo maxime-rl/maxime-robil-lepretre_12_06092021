d8c10208e1ff04601460c2640e099d3f3f270099
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-09-12 17:00:55 +0200

    feat: add basic Switch and Routes to test routing

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavTopBar, NavActivities } from "../components";
 
 function App() {
   return (
-    <>
+    <Router>
       <NavTopBar />
       <NavActivities />
-    </>
+      <Switch>
+        <Route exact path="/user/12">
+          <h1>user 12</h1>
+        </Route>
+        <Route path="/user/18">
+          <h1>user 18</h1>
+        </Route>
+        <Route path="*">
+          <h1>Error</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
