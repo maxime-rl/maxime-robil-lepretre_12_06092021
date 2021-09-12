5c8ca2a40aad979ea67a2ba5eb0b3a7b97b15c8e
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-09-12 12:20:59 +0200

    feat: add GlobalStyle and project variables

---
+++ src/utils/styles/variables.js
@@ -0,0 +1,9 @@
+export const variablesStyle = {
+  colorPrimary: "#FF0000",
+  colorGray: "#74798C",
+  colorGrayDarken: "#282D30",
+  colorGrayLighten: "#FBFBFB",
+  colorBlack: "#020203",
+  colorWhite: "#FFFFFF",
+  radius: "5px",
+};
