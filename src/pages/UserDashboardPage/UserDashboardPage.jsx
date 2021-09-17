48958669bdcc102bdc9b0561c88496ec56ed778f
Maxime Robil-Leprêtre <maxime.robil.leprtre@gmail.com>
2021-09-17 13:40:59 +0200

    feat: add basic fetch sportSee API with services switch

--- src/pages/UserDashboardPage/UserDashboardPage.jsx
+++ src/pages/UserDashboardPage/UserDashboardPage.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { NavActivities, Greeting } from "../../components";
 import * as S from "./UserDashboardPage.styled";
+import { getUserFetchData } from "../../services/fetchSportSeeAPI";
 
 export default function UserDashboardPage() {
+  const [data, setData] = useState([]);
   let { userId } = useParams();
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await getUserFetchData(userId);
+      if (!response) return console.log("response error");
+
+      setData(response.data);
+    };
+    getData();
+  }, [userId]);
+
+  if (data.length === 0) return null;
 
   return (
     <>
       <NavActivities />
       <S.header>
-        <Greeting firstName={userId} />
+        <Greeting firstName={data.userInfos.firstName} />
       </S.header>
     </>
   );
